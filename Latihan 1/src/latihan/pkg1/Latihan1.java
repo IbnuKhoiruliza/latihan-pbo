49e61556ca14ebc3cab223c6cde53f8fb048261b
IbnuKhoiruliza <116357174+IbnuKhoiruliza@users.noreply.github.com>
2022-10-23 22:10:49 +0700

    Add files via upload

---
+++ Latihan 1/src/latihan/pkg1/Latihan1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package latihan.pkg1;
+
+/**
+ *
+ * @author DIPLONG
+ */
+public class Latihan1 {
+    String nama1, nama2, jeniskelamin1, jeniskelamin2;
+    int umur1,umur2;
+    
+    void Latihan1() {
+        System.out.println("nama1");
+        System.out.println("nama                : "+nama1);
+        System.out.println("jenis kelamin       : "+jeniskelamin1);
+        System.out.println("umur                : "+umur1);
+        System.out.println("nama2");
+        System.out.println("nama                : "+nama2);
+        System.out.println("jenis kelamin       : "+jeniskelamin1);
+        System.out.println("umur                : "+umur1);
+    }
+}
---
+++ Latihan 1/src/latihan/pkg1/atribut.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihan.pkg1;
+
+/**
+ *
+ * @author DIPLONG
+ */
+public class atribut {
+    public static void main(String[] args){
+        
+        Latihan1 orang = new Latihan1();
+        
+        orang.nama1="anton";
+        orang.nama2="riko";
+        orang.jeniskelamin1="Laki-Laki";
+        orang.jeniskelamin2="Laki-Laki";
+        orang.umur1=20;
+        orang.umur2=21;
+        
+        orang.Latihan1();
+        
+        
+    }
+    
+}
